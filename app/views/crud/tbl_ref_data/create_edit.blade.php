2ae2a34bc791f323608ff23c1ae97153cfb4b6a8
i2x <auue54@gmail.com>
2014-09-02 00:23:20 +0700

    CRUD VIEW

---
+++ app/views/crud/tbl_ref_data/create_edit.blade.php
@@ -0,0 +1,122 @@
+@extends('submenu')
+
+{{-- Content --}}
+@section('content')
+
+@if($ampher_message != NULL){{$ampher_message}}
+@endif
+
+
+				
+
+@if(isset($data['AMPHUR_ID']))
+{{ Form::open(array('url' => URL::to('database/amphur/'.$data['AMPHUR_ID'].'/update') )) }}
+@else
+{{ Form::open(array('url' => URL::to('database/amphur/create') )) }}
+@endif
+
+	
+   <div class="form-group">
+  {{ Form::label('AMPHUR_CODE')}}
+  @if(isset($data['AMPHUR_CODE']))
+  
+  {{ Form::text('AMPHUR_CODE', $data['AMPHUR_CODE'],array( 'class' => 'form-control', 'placeholder' => 'Enter Code'  )) }}
+ 
+  @else
+ 
+  {{ Form::text('AMPHUR_CODE','',array( 'class' => 'form-control', 'placeholder' => 'Enter Code'  )) }}
+ 
+  @endif
+
+  <p class="text-danger">{{$errors->first('AMPHUR_CODE')}}</p>
+   
+  </div>
+  
+  
+  <div class="form-group">
+  {{ Form::label('AMPHUR_NAME')}}
+  @if(isset($data['AMPHUR_NAME']))
+  {{
+  	 Form::text('AMPHUR_NAME', $data['AMPHUR_NAME'],
+ 	 array( 'class' => 'form-control', 'placeholder' => 'Enter Name'  ))
+  }}
+  @else
+  {{ 
+  
+  	Form::text('AMPHUR_NAME', '',
+ 	array( 'class' => 'form-control', 'placeholder' => 'Enter Name'  ))
+  }}
+  @endif
+  <p class="text-danger">{{$errors->first('AMPHUR_NAME')}}</p>
+    </div>
+
+  
+  
+  
+  
+     <div class="form-group">
+  {{ Form::label('GEO_ID')}}
+  @if(isset($data['GEO_ID']))
+  {{
+  	 Form::text('GEO_ID', $data['GEO_ID'],
+ 	 array( 'class' => 'form-control', 'placeholder' => 'Enter ID'  ))
+  }}
+  @else
+  {{ 
+  
+  	Form::text('GEO_ID', '',
+ 	array( 'class' => 'form-control', 'placeholder' => 'Enter ID'  ))
+  }}
+  @endif
+  <p class="text-danger">{{$errors->first('GEO_ID')}}</p>
+    
+  </div>
+  
+  
+  
+  
+     <div class="form-group">
+  {{ Form::label('PROVINCE_ID')}}
+  @if(isset($data['PROVINCE_ID']))
+  {{
+  	 Form::text('PROVINCE_ID', $data['PROVINCE_ID'],
+ 	 array( 'class' => 'form-control', 'placeholder' => 'Enter ID'  ))
+  }}
+  @else
+  {{ 
+  
+  	Form::text('PROVINCE_ID', '',
+ 	array( 'class' => 'form-control', 'placeholder' => 'Enter ID'  ))
+  }}
+  @endif
+  <p class="text-danger">{{$errors->first('PROVINCE_ID')}}</p>
+  
+  
+  </div>
+  
+  
+  
+
+  
+  	<div class="form-group">
+		
+				<!--  <element class="btn btn-default close_popup">Cancel</element>-->
+				
+				<button type="submit" class="btn btn-success">
+				{{($mode == 'Create') ? 'Create' : 'Update'}}
+				</button>
+				
+				<button type="reset" class="btn btn-default">
+				
+				<span class="glyphicon glyphicon-repeat"></span> Reset
+				
+				</button>
+		</div>
+ 
+
+
+{{ Form::close() }}
+@stop
+
+
+
---
+++ app/views/crud/tbl_ref_settings/table.blade.php
@@ -0,0 +1,122 @@
+@extends('protected.admin.master')
+
+@section('title', 'CRUD - Ampur')
+
+@section('content')
+
+@if (Session::has('flash_message'))
+			<p>{{ Session::get('flash_message') }}</p>
+	@endif
+
+
+	<ol class="breadcrumb">
+  	<li><a href="{{URL::to('database')}}">Database</a></li>
+  	<li class="active">Ampur</li>
+	</ol>
+	
+	
+	
+
+			<div class="pull-right">
+				<a href="{{{ URL::to('database/amphur/create') }}}" class="btn btn-small btn-info iframe">
+				<span class="glyphicon glyphicon-plus-sign"></span> Create</a>
+			</div>
+			<br>
+		
+	
+	
+	 <div class="page-header">
+	
+	
+	</div>
+	
+			
+						<table id="gridview" class="table table-striped table-hover table-condensed " >
+						<thead>
+						
+								<tr>
+									<th class="col-md-1">id</th>
+									<th class="col-md-1">name</th>
+									<th class="col-md-1">permissions</th>
+									<th class="col-md-1">created_at</th>
+									<th class="col-md-1">updated_at</th>
+									<th class="col-md-1"> </th>
+
+								
+								</tr>
+							</thead>
+						</table>
+
+		
+ 
+
+@endsection
+
+
+
+{{-- Style --}}
+
+@section('style')
+	
+	{{ HTML::style('packages/datatables/css/dataTables.bootstrap.css')}}
+	{{ HTML::style('packages/colorbox/css/colorbox.css')}}
+	
+
+
+@endsection
+
+{{-- Scripts --}}
+@section('scripts')
+
+
+	{{ HTML::script('packages/jquery/jquery.min.js'); }}
+	{{ HTML::script('packages/bootstrap/js/bootstrap.min.js'); }}
+    {{ HTML::script('packages/datatables/js/jquery.dataTables.min.js')}}
+	{{ HTML::script('packages/datatables/js/dataTables.bootstrap.js')}}
+	{{ HTML::script('packages/colorbox/js/jquery.colorbox.js')}}
+	{{ HTML::script('packages/datatables/js/fnReloadAjax.js')}}
+	{{ HTML::script('packages/bootbox/bootbox.min.js')}}
+	
+
+
+	<script type="text/javascript">
+		var oTable;
+		$(document).ready(function() {
+			
+			oTable = $('#gridview').dataTable( {
+				"sDom": "<'row'<'col-md-6'l><'col-md-6'f>r>t<'row'<'col-md-6'i><'col-md-6'p>>",
+				
+				"oLanguage": {
+					"sLengthMenu": "_MENU_ records per page"
+								},
+				"bProcessing": true,
+		        "bServerSide": true,
+		        "iDisplayLength": 25,
+		        "sAjaxSource": "{{ URL::to('database/groups/data') }}",
+		        "fnDrawCallback": function ( oSettings ) {
+	           	$(".iframe").colorbox({iframe:true, transition:"none", width:"80%", height:"80%", escKey: false,
+	           		    overlayClose: false});
+	     		}
+			});
+
+		});
+		function Delete(id) {
+			bootbox.confirm("The <code> AMPHUR ID "+id+" </code>will be deleted immediately. Are you sure you want to continue?", 
+			function(result) {
+				if(result){
+					 $.ajax( 
+							 {
+
+							    url: 'amphur/'+id+'/delete',
+							    type: 'POST',
+							    success: function(result) {
+							   	 	parent.oTable.fnReloadAjax(null,null,true);
+							    }
+							  });
+					  }}); 
+			  }
+		  </script>
+@endsection
+
+
+	
