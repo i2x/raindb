3532f201c3fe4c854ee027cecece877ea19a2f32
i2x <auue54@gmail.com>
2014-09-05 01:01:00 +0700

    update

--- app/controllers/ScheduleController.php
+++ app/controllers/ScheduleController.php
@@ -15,19 +15,20 @@ class ScheduleController extends BaseController {
             
         }
 
-
-
-
-
             return View::make('schedule.schedule')->with('files', $temp);
         }
 
 
         
 
-        function postIndex() {
+        function postIndex() 
+        
+        {
             //TODO
-            $this->actionUpdate();
+
+        	
+        	
+        	$this->actionUpdate();
 		
 		return $this->getIndex();
         }
@@ -36,20 +37,20 @@ public function actionUpdate()
 	{
 		
 		$update = 'Update';
-		//$this->Moveto_upload_file();
+		//$this->Moveto_upload_files();
 		
 		$files = $this->FileList();
 		$path = '';
 		foreach ($files as $file)
 		{
 			
-			$path = base_path() . DIRECTORY_SEPARATOR . 'schedule'.DIRECTORY_SEPARATOR.$file;
 			
 			if(substr($file,-3) == "xls")
 			{
 				
-				$this->_excel($path);$type = ".xls";
-				$this->Moveto_upload_file($file,$type);
+				
+				$this->push_temp($file);
+				$this->_excel($file);
 				
 				
 				
@@ -57,7 +58,7 @@ public function actionUpdate()
 			elseif (substr($file,-3) == "txt")
 			{
 				$this->_text($path);$type = ".txt";
-				$this->Moveto_upload_file($file,$type);
+				$this->Moveto_upload_files($file,$type);
 				
 				
 				
@@ -75,7 +76,7 @@ public function actionUpdate()
 		
 		$from = base_path() . DIRECTORY_SEPARATOR . 'schedule'.DIRECTORY_SEPARATOR;
 		
-	    $this->render('schedule',array('update' => $update,'temp' => $path));
+		return View::make('schedule.schedule');
 				
 	}
 	public function FileList()
@@ -96,31 +97,20 @@ public function actionUpdate()
 	}
 	
 	
-	public function Moveto_upload_file($file,$type)
+	public function Moveto_upload_files($file,$type)
 	{
 		
 		
 		$new_name = $file; 
-		
-		if(array_search($file, scandir(base_path() . DIRECTORY_SEPARATOR . 'upload_file')))
-		{
-		
-		$new_name =substr($file,0,-4).date('Y-m-d His').$type;
-	
-		
-		}
-		
+		if(array_search($file, scandir(base_path() . DIRECTORY_SEPARATOR . 'upload_files')))$new_name =substr($file,0,-4).date('Y-m-d His').$type;
 		$from = base_path() . DIRECTORY_SEPARATOR . 'schedule'.DIRECTORY_SEPARATOR;
-	    $to   = base_path() . DIRECTORY_SEPARATOR . 'upload_file'.DIRECTORY_SEPARATOR;
+	    $to   = base_path() . DIRECTORY_SEPARATOR . 'upload_files'.DIRECTORY_SEPARATOR;
 	    
+	    $query = array(
+	    		'filename' => $new_name,
+	    		'detail' => 'update from schedule');
 
-	    $sql = "INSERT INTO `rain`.`tbl_import_log` ( `importdate`, `filename`, `detail`)
-                			VALUES ( CURRENT_TIMESTAMP, '".$new_name."','update from schedule');";
-	    DB:table('tbl_import_log')->insert(
-                    array(
-                        importdate
-                    )
-                    );
+	    DB::table('tbl_import_log')->insert($query);
 	
 		$success = rename($from.$file, $to.$new_name);
 				
@@ -128,142 +118,130 @@ public function actionUpdate()
 	
 
 	
-	public function _excel($path){
+	public function _excel($fileName){
 		
 		
-			$objPHPExcel = PHPExcel_IOFactory::load($path);
+		File::move(base_path('schedule/').$fileName, base_path('upload_files/').$fileName);//Move File Form 'temp' To 'uploadFiles'UploadFiles
 		
-			$worksheet = $objPHPExcel->getActiveSheet();
-			$highestRow = $worksheet->getHighestRow() . '<br>';
-			for ($row = 2; $row <= $highestRow; ++$row) {
-				$cell = $worksheet->getCellByColumnAndRow(1, $row);
-				$sta_id = $cell->getValue();
-				$cell = $worksheet->getCellByColumnAndRow(2, $row);
-				$dyear = $cell->getValue();
-				$cell = $worksheet->getCellByColumnAndRow(3, $row);
-				$dmonth = $cell->getValue();
-				$cell = $worksheet->getCellByColumnAndRow(4, $row);
-				$dday = $cell->getValue();
-				$cell = $worksheet->getCellByColumnAndRow(5, $row);
-				$maxtmp = $cell->getValue();
-				$cell = $worksheet->getCellByColumnAndRow(6, $row);
-				$mintmp = $cell->getValue();
-				$cell = $worksheet->getCellByColumnAndRow(7, $row);
-				$rain = $cell->getValue();
-				$cell = $worksheet->getCellByColumnAndRow(8, $row);
-				$avgrh = $cell->getValue();
-				$cell = $worksheet->getCellByColumnAndRow(9, $row);
-				$evapor = $cell->getValue();
-				$cell = $worksheet->getCellByColumnAndRow(10, $row);
-				$meantemp = $cell->getValue();
-		
-				if( $row < $highestRow) // push some data for preview
-				{
-		
-					
-					try {
-						$result =  DB::table('tbl_rain_measurement')->insert(
-                                                        array(
-								'station_id' => $sta_id,
-								'meas_date' => $dyear . '-' . $dmonth . '-' . $dday,
-								'max_temp' => $maxtmp,
-								'min_temp' => $mintmp,
-								'rain' => $rain,
-								'avgrh' => $avgrh,
-								'evapor' => $evapor,
-								'mean_temp' => $meantemp,
-								'source' => 1 //source from excel
+		ImportLog::insert(array(
+			'filename'   => $fileName,
+			'detail'     => 'import by schedule',
+			
+		));		
+		$source = ImportPreview::get();
+		$insert = 0;
+		$update = 0;
+		foreach ($source as $source)
+		{ 
+			$target = DB::table('tbl_rain_measurement')
+			->where('station_id',$source['station_id'])
+			->where('meas_date',$source['meas_date'])
+			->where('source',$source['source']);
+			if($target->exists())
+			{
+				$target->update(
+						array(
+									
+								'max_temp' => $source['max_temp'],
+								'min_temp' => $source['min_temp'],
+								'rain' => $source['rain'],
+								'avgrh' => $source['avgrh'],
+								'mean_temp' => $source['mean_temp'],
+								'meas_year' => $source['meas_year'],
+								'max_temp' => $source['max_temp'],
+								'meas_month' => $source['meas_month'],
+								'meas_day' => $source['meas_day'],
 						));
-					} catch (\Exception $exc) {
-						//echo $exc->getTraceAsString();
-					}
-				}
+				$update++;
+			}
+			else
+			{
+				$target->insert(
+						array(
+								'station_id'=>$source['station_id'],
+								'meas_date'=>$source['meas_date'],
+								'source'=>$source['source'],
+								'max_temp' => $source['max_temp'],
+								'min_temp' => $source['min_temp'],
+								'rain' => $source['rain'],
+								'avgrh' => $source['avgrh'],
+								'mean_temp' => $source['mean_temp'],
+								'meas_year' => $source['meas_year'],
+								'max_temp' => $source['max_temp'],
+								'meas_month' => $source['meas_month'],
+								'meas_day' => $source['meas_day'],
+						));
+				
+				$insert++;
+		
 			}
 		
-		
-		
-		
+		}
+
+				
 	}
 	
+	public function push_temp($fileName)
+	{
+		DB::table('tbl_temp_measurement')->truncate();
+		Excel::load(base_path('schedule/'.$fileName), function($reader) {
+			$results = $reader->toArray();
+			$arr = array();
+		
+			try {
+		
+				foreach ($results[0] as $value)
+				{
+					if(isset($value['year']) || isset($value['rain']) )
+					{
+						$temp = array(
+		
+								'meas_date'   => $value['year'].'-'.$value['month'].'-'.$value['dday'],
+								'station_id'  => $value['stncode'],
+								'max_temp' 	  => $value['maxtmp'],
+								'min_temp' 	  => $value['mintmp'],
+								'rain' 		  => $value['rain'],
+								'avgrh' 	  => $value['avgrh'],
+								'evapor'      => $value['evapor'],
+								'mean_temp'	  => $value['meantemp'],
+								'source'	  => 1,
+								'meas_year'   => $value['year'],
+								'meas_month'  => $value['month'],
+								'meas_day'    => $value['dday'],
+						);
+		
+						array_push($arr, $temp);
+					}}
+						
+					try {
+		
+						DB::table('tbl_temp_measurement')->insert($arr);
+		
+		
+					}
+					catch (Exception $e)
+					{
+		
+						return Redirect::to('import')->with('message' , 'file incorrect	');
+						
+					}
+			}
+			catch (Exception $e)
+			{
+				// file incorrect
+		
+				return Redirect::to('import')->with('message' , 'file incorrect	');
+		
+		
+			}
+		
+		
+		
+		});
+	}
+
 	
-    public function _text($path){
-		
-		
-		$lines = file($path);
-		
-		
-		$month= array("04","05","06","07","08","09","10","11","12","01","02","03");
-		
-		
-		$temp = array();
-		$year = 0;
-		$meas_date = "";
-		$day = 0;
-		$LOG = array();
-		$frist = true;
-		
-		$sql = "INSERT INTO `rain`.`tbl_rain_measurement` ( `meas_date`, `station_id`, `rain`, `source`) VALUES";
-		foreach ($lines as $line_num => $line)
-		{
-		
-		
-
-			$page = floor($line_num/61);
-			if ($line_num == 6+61*$page)
-			{
-				$year = substr($line,65,4);
-			}
-			if ($line_num == 4+61*$page)
-			{
-				$station_id = substr($line,14,4);
-			}
-		
-			if($line_num >= 14+61*$page && $line_num <= 46+61*$page && $line_num != 24+61*$page && $line_num != 35+61*$page)
-			{
-				 
-				if($day > 31)$day =0;$day++;
-				for ($column =0;$column <12;$column++)
-				{
-		
-				$meas_date = $year."-".$month[$column] ."-".$day;
-				if(checkdate((int)$month[$column],(int)$day, (int)$year))
-				{
-				$rain = substr($line,13+8*$column,8);
-				if($rain == "        ")array_push($LOG,$meas_date."    ".'NULL');
-				else if ($rain == "      - ")array_push($LOG,$meas_date."    ".'N/A');
-				else
-				{
-				if($frist)
-				{
-		
-				$sql = $sql."('".$meas_date."','".(int)$station_id."','".(float)$rain."','3')";
-				$frist = false;
-				}
-				else  $sql = $sql.",('".$meas_date."','".(int)$station_id."','".(float)$rain."','3')";
-				 
-				}
-				}
-		
-				}
-				}
-				 
-				}
-				
-				try {
-				Yii::app()->db->createCommand($sql)->execute();
-				}
-				catch (CDbException $e)
-				{
-					echo $e->getTraceAsString();
-					
-				}
-
-		
-		        foreach ($LOG as $key => $value)
-				{
-		
-                }
-		
-		}        
+	
+   
 
     }
