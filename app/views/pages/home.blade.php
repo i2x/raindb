8204195108b663487dfd0989f1485968cb4517b9
i2x <auue54@gmail.com>
2014-09-14 21:19:53 +0700

    home

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -8,8 +8,14 @@
         <div class="col-lg-12">
             <div class="jumbotron">
                 <h1>Welcome! </h1>
-                <p class="lead">Welcome to seasonal forecast system. Please login or sign up. </p>
-                <p><a class="btn btn-lg btn-success" href="#" role="button">Lean more.</a></p>
+                <p class="lead">Welcome to seasonal forecast system. Please  <a href="{{URL::to('login')}}">login</a> or 
+                <a href="{{URL::to('register')}}"> sign up.</a> 
+                
+                </p>
+                
+                
+               
+                
             </div>
         </div>
     </div>
